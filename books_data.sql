d78e889f629175b619a8a96bf71a67ae1bfcbb8c
Thom Null <raulesquerra18@gmail.com>
2025-07-12 20:08:21 -0700

    afk for a bit, pushing current work

---
+++ books_data.sql
@@ -0,0 +1,34 @@
+-- books_data.sql
+-- changed foramt from single char to full text
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('Waiting for God', 'Simone Weil', '978-0-06-171896-0', 1950, 'Philosophy', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('A Random Walk Down Wall Street', 'Burton G. Malkiel', '978-0-393-35838-4', 1973, 'Reference Work', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('Personal Investing', 'Wilbur W. Widicus & Thomas E. Stitzel', '0-256-01832-4', 1971, 'Reference Work', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('The Myth of Sisyphus', 'Albert Camus', '978-0-525-56445-4', 1942, 'Philosophy', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('The Plague', 'Albert Camus', '0-679-72021-9', 1947, 'Philosophy', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('The Design of Everyday Things', 'Don Norman', '978-0-465-05065-9', 1988, 'Psychology', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('Operating Systems Three Easy Pieces', 'Remzi H. Arpaci-Dusseau Andrea C. Arpaci-Dusseau', '978-1985086593', 2012, 'Computer Science', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('Learn Python the Hard Way Fifth Edition', 'Zed A. Shaw', '978-0-13-827057-5', 2024, 'Computer Science', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('Principles of Programming Languages Design, Evaluation, and Implementation Third Edition', 'Bruce J. Maclennan', '0-19-511306-3', 1999, 'Computer Science', 'Digital', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('Introduction to Java Programming Comprehensive version 10th Edition', 'Y. Daniel Liang', '978-0-13-376131-3', 2014, 'Computer Science', 'Physical', 'Shelf 2');
+
+INSERT INTO books (title, author, isbn, publication_year, genre, format, storage_location) VALUES
+('Where Are You When I Need You?', 'Suzanne Newton', '0-670-81702-3', 1991, 'Fiction', 'Physical', 'Shelf 2');
